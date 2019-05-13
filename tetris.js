3b3ef1e77e309e87dae24383c4d8f1af86b9c010
pppt94 <43952779+pppt94@users.noreply.github.com>
2019-05-13 11:50:47 +0200

    Add draw_board & draw_square  functions

--- tetris.js
+++ tetris.js
@@ -1,5 +1,39 @@
-const canvas = document.getElementById('tetris');
-const context = canvas.getContext('2d');
+const cvs = document.getElementById("tetris");
+const ctx = cvs.getContext("2d");
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+const ROW = 20;
+const COL = 10;
+const block_size = 20;
+
+function draw_square(x, y, color){
+    
+    ctx.fillStyle = color;
+    ctx.fillRect(x*block_size, y*block_size, block_size, block_size);
+
+    ctx.strokeStyle = "BLACK";
+    ctx.strokeRect(x*block_size, y*block_size, block_size, block_size);
+}
+
+let board = [];
+
+function create_board()
+{
+    for(x = 0; x < ROW; x++){
+        board[x] = [];
+        for(y = 0; y < COL; y++){
+            board[x][y] = "white";
+        }
+    }
+}
+
+function draw_board()
+{
+    for(x = 0; x < ROW; x++){
+        for(y = 0; y < COL; y++){
+            draw_square(y, x, board[x][y]);
+        }
+    }
+}
+
+create_board();
+draw_board();
