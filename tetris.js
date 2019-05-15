d48f7c25a46e65c1251e85a6cd8c4502d50493e0
pppt94 <43952779+pppt94@users.noreply.github.com>
2019-05-15 10:10:08 +0200

    Add Block class

--- tetris.js
+++ tetris.js
@@ -5,16 +5,133 @@ const ROW = 20;
 const COL = 10;
 const block_size = 20;
 
+var I = [['..X..',
+      '..X..',
+      '..X..',
+      '..X..',
+      '.....'],
+     ['.....',
+      'XXXX.',
+      '.....',
+      '.....',
+      '.....']]
+
+var O = [['.....',
+      '.....',
+      '.XX..',
+      '.XX..',
+      '.....']]
+
+var S = [['.....',
+      '.....',
+      '..XX.',
+      '.XX..',
+      '.....'],
+     ['.....',
+      '..X..',
+      '..XX.',
+      '...X.',
+      '.....']]
+
+var Z = [['.....',
+      '.....',
+      '.XX..',
+      '..XX.',
+      '.....'],
+     ['.....',
+      '..X..',
+      '.XX..',
+      '.X...',
+      '.....']]
+
+var T = [['.....',
+      '..X..',
+      '.XXX.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..X..',
+      '..XX.',
+      '..X..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.XXX.',
+      '..X..',
+      '.....'],
+     ['.....',
+      '..X..',
+      '.XX..',
+      '..X..',
+      '.....']]
+
+var J = [['.....',
+      '..X..',
+      '..X..',
+      '.XX..',
+      '.....'],
+     ['.....',
+      '.X...',
+      '.XXX.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..XX.',
+      '..X..',
+      '..X..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.XXX.',
+      '...X.',
+      '.....']]
+
+var L = [['.....',
+      '..X..',
+      '..X..',
+      '..XX.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.XXX.',
+      '.X...',
+      '.....'],
+     ['.....',
+      '.XX..',
+      '..X..',
+      '..X..',
+      '.....'],
+     ['.....',
+      '...X.',
+      '.XXX.',
+      '.....',
+      '.....']]
+
+
+var shapes = [I, O, S, Z, T, J, L]
+var colors = ["red", "green", "blue", "gold", "orange", "gray", "brown"]
+
+class Block {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.shape = shapes[Math.floor(Math.random() * shapes.length)];
+        this.color = colors[shapes.findIndex(x => x == this.shape)]
+        this.rotation = 0;
+    }
+}
+
+const block = new Block(2, 2);
 function draw_square(x, y, color){
     
     ctx.fillStyle = color;
     ctx.fillRect(x*block_size, y*block_size, block_size, block_size);
 
-    ctx.strokeStyle = "BLACK";
+    ctx.strokeStyle = "black";
     ctx.strokeRect(x*block_size, y*block_size, block_size, block_size);
 }
 
-let board = [];
+var board = [];
 
 function create_board()
 {
