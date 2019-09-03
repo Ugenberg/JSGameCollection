185adfff8f949e4f1b1d56b94b5ec0a082f0d335
pppt94 <43952779+pppt94@users.noreply.github.com>
2019-09-03 17:44:01 +0200

    Fix names bug

--- snake.js
+++ snake.js
@@ -1,4 +1,4 @@
-const cvs = document.getElementById("snake");
+const cvs = document.getElementById("snake_canv");
 const ctx = cvs.getContext("2d");
 
 let block_size = 20;
@@ -13,39 +13,51 @@ function draw_square(x, y, color, ctx){
 }
 
 
-
-        
-function draw_board() {
-    for(var x = 0; x < 15; x++){
-        for(var y = 0; y < 25; y++){
-            
-            if(y%2==0 && x%2==1)
-                draw_square(y, x, "darkgreen", ctx);
-            else
-                draw_square(y, x, "green", ctx);
-        }
+class Board {
+    constructor() {
+        this.draw_board();
+    }
+    draw_board() {
+        for(var x = 0; x < 15; x++){
+            for(var y = 0; y < 25; y++){
+                if(y%2==0 && x%2==1)
+                    draw_square(y, x, "darkgreen", ctx);
+                else
+                    draw_square(y, x, "green", ctx);
+            }
+        } 
     }
 }
 
-draw_board();
-
-let snake = [];
-
-snake[0] = {
-    x : 9 * block_size,
-    y : 10 * block_size
-};
-
-function draw_snake(){
-
-    for( let i = 0; i < snake.length ; i++){
-        ctx.fillStyle = ( i == 0 )? "blue" : "white";
-        ctx.fillRect(snake[i].x,snake[i].y,block_size,block_size);
-
-        ctx.strokeStyle = "red";
-        ctx.strokeRect(snake[i].x,snake[i].y,block_size,block_size);
-    }    
+class Snake {
+    constructor(board) {
+        this.body = [];
+        this.board = board;
+        this.create_head();
+        
+    }
+    
+    create_head() {
+        
+        this.body[0] = {x: 10 * block_size,
+                        y: 10 * block_size};
+    }
+    
+    update_position() {
+        for( let i = 0; i < this.body.length ; i++){
+            ctx.fillStyle = ( i == 0 )? "blue" : "white";
+            ctx.fillRect(this.body[i].x,this.body[i].y,block_size,block_size);
+            ctx.strokeStyle = "red";
+            ctx.strokeRect(this.body[i].x,this.body[i].y,block_size,block_size);
+        }    
+    
+    }
 }
+
+
+
+var board = new Board();
+var snake = new Snake(board);
 
 
 document.addEventListener("keydown",direction);
@@ -53,14 +65,20 @@ document.addEventListener("keydown",direction);
 function direction(event){
     let key = event.keyCode;
     if( key == 37){
-        snake[0].x-=block_size;
+        snake.body[0].x-=block_size;
     }else if(key == 38){
-        snake[0].y-=block_size;
+        snake.body[0].y-=block_size;
     }else if(key == 39){
-        snake[0].x+=block_size;
+        snake.body[0].x+=block_size;
     }else if(key == 40){
-        snake[0].y+=block_size;
+        snake.body[0].y+=block_size;
     }
 }
 
-let game = setInterval(draw_snake,100);
+setInterval(snake.update_position,100);
+
+
+
+function quitGame() {
+    window.location.href='menu.html';
+}
