ab375b5f1615a645292d0a166841d0a82f3a42f6
pppt94 <43952779+pppt94@users.noreply.github.com>
2019-09-04 11:55:29 +0200

    Add basic logic of snake game

--- snake.js
+++ snake.js
@@ -29,28 +29,93 @@ class Board {
     }
 }
 
+class Food {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.reset_food();
+    }
+    
+    draw_food() {
+        draw_square(this.x, this.y, "red", ctx);
+    }
+    
+    reset_food() {
+        this.x = Math.floor(Math.random() * (25 - 0 + 1));
+        this.y = Math.floor(Math.random() * (15 - 0 + 1));
+        this.draw_food();
+    }
+}
+
 class Snake {
     constructor(board) {
         this.body = [];
         this.board = board;
         this.create_head();
+        this.direction = "";
         
     }
     
     create_head() {
         
-        this.body[0] = {x: 10 * block_size,
-                        y: 10 * block_size};
+        this.body[0] = [10, 10];
+    }
+    
+    draw_snake() {
+        for( let i = 0; i < this.body.length ; i++){
+            draw_square(this.body[i][0], this.body[i][1], "blue", ctx);
+        }
     }
     
     update_position() {
-        for( let i = 0; i < this.body.length ; i++){
-            ctx.fillStyle = ( i == 0 )? "blue" : "white";
-            ctx.fillRect(this.body[i].x,this.body[i].y,block_size,block_size);
-            ctx.strokeStyle = "red";
-            ctx.strokeRect(this.body[i].x,this.body[i].y,block_size,block_size);
-        }    
+        
+        this.board.draw_board();
+        this.draw_snake();
+        food.draw_food();
+        let bol = this.eat_food();
+        
+        if(!bol)
+            this.move_body();
+        this.move_head();
+        
+        
+    }
     
+    move_head() {
+        if(this.direction == "LEFT"){
+            this.body[0][0]--;
+        }
+        else if(this.direction == "RIGHT"){
+            this.body[0][0] += 1;
+        }
+        else if(this.direction == "UP"){
+            this.body[0][1]--;
+        }
+        else if(this.direction == "DOWN"){
+            this.body[0][1]++;
+        }
+        
+    }
+    
+    move_body() {
+        
+        for(let i = this.body.length - 1; i >= 1; i--){
+            this.body[i][0] = this.body[i-1][0];
+            this.body[i][1] = this.body[i-1][1];
+        }
+        
+    }
+    
+    eat_food() {
+        
+        console.log(this.body[0], food.x, food.y);
+        
+        if(this.body[0][0] == food.x && this.body[0][1] == food.y) {
+            this.body.push([food.x, food.y]);
+            food.reset_food();
+            return true;
+        }
+        return false;
     }
 }
 
@@ -58,6 +123,8 @@ class Snake {
 
 var board = new Board();
 var snake = new Snake(board);
+var food = new Food();
+
 
 
 document.addEventListener("keydown",direction);
@@ -65,17 +132,17 @@ document.addEventListener("keydown",direction);
 function direction(event){
     let key = event.keyCode;
     if( key == 37){
-        snake.body[0].x-=block_size;
+        snake.direction = "LEFT";
     }else if(key == 38){
-        snake.body[0].y-=block_size;
+        snake.direction = "UP";
     }else if(key == 39){
-        snake.body[0].x+=block_size;
+        snake.direction = "RIGHT";
     }else if(key == 40){
-        snake.body[0].y+=block_size;
+        snake.direction = "DOWN";
     }
 }
 
-setInterval(snake.update_position,100);
+setInterval(() => snake.update_position(),100);
 
 
 
