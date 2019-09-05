4cdd31a2faf5a1c221b6e04f223d5bd2f677517e
pppt94 <43952779+pppt94@users.noreply.github.com>
2019-09-05 12:51:09 +0200

    Add level & obstacles to snake

--- snake.js
+++ snake.js
@@ -10,6 +10,9 @@ const headImg = new Image();
 headImg.src = "img/head.png";
 const backImg = new Image();
 backImg.src = "img/back.png";
+var scr = document.getElementById("score_s");
+var lvl = document.getElementById("level_s");
+var ets = document.getElementById("lines_s");
 
 function draw_square(x, y, color, ctx){
     
@@ -24,9 +27,27 @@ function draw_square(x, y, color, ctx){
 class Board {
     constructor() {
         this.draw_board();
+        this.level = 1;
+        this.obstacles = [[[11, 6],[12,6],[13,6],[11, 7],[12,7],[13,7],[11, 8],[12,8],[13,8]],
+                          [[4, 6],[5,6],[6,6],[4, 7],[5,7],[6,7],[4, 8],[5,8],[6,8],[18, 6],[19,6],[20,6],[18, 7],[19,7],[20,7],[18, 8],[19,8],[20,8]],
+                          [[4, 6],[5,6],[6,6],[4, 7],[5,7],[6,7],[4, 8],[5,8],[6,8],[18, 6],[19,6],[20,6],[18, 7],[19,7],[20,7],[18, 8],[19,8],[20,8],[11, 6],[12,6],[13,6],[11,7],[12,7],[13,7],[11, 8],[12,8],[13,8],[10,6],[10,7],[10,8],[14,6],[14,7],[14,8]],
+                          [[4, 6],[5,6],[6,6],[4, 7],[5,7],[6,7],[4, 8],[5,8],[6,8],[18, 6],[19,6],[20,6],[18, 7],[19,7],[20,7],[18, 8],[19,8],[20,8],[11, 6],[12,6],[13,6],[11,7],[12,7],[13,7],[11, 8],[12,8],[13,8],[10,6],[10,7],[10,8],[14,6],[14,7],[14,8],[10,5],[11,5],[12,5],[13,5],[14,5],[10,9],[11,9],[12,9],[13,9],[14,19]]];
     }
     draw_board() {
         ctx.drawImage(backImg, 0, 0);
+        if(this.level > 1){
+            this.create_obstacles();
+        }
+    }
+    
+    create_obstacles() {
+        
+        var idx = this.level <= 5 ? this.level - 2 : 5;
+        
+        for(var i = 0; i < this.obstacles[idx].length; i++) {
+            draw_square(this.obstacles[idx][i][0], this.obstacles[idx][i][1], "brown", ctx);    
+        }
+        
     }
 }
 
@@ -49,10 +70,8 @@ class Food {
 
 class Snake {
     constructor(board) {
-        this.body = [];
         this.board = board;
-        this.create_head();
-        this.direction = "";
+        this.reset_snake();
         
     }
     
@@ -74,6 +93,14 @@ class Snake {
         this.body = [];
         this.direction = "";
         this.create_head();
+        this.level = 1;
+        board.level = this.level;
+        this.eats = 0;
+        this.score = 0;
+        this.limit = this.level * 50;
+        scr.innerHTML = this.score;
+        ets.innerHTML = this.eats;
+        lvl.innerHTML = this.level;
     }
     
     update_position() {
@@ -124,12 +151,27 @@ class Snake {
         if(this.body[0][0] == food.x && this.body[0][1] == food.y) {
             this.body.splice(1,0,[food.x, food.y]);
             food.reset_food();
+            this.score += 10;
+            this.eats += 1;
+            scr.innerHTML = this.score;
+            ets.innerHTML = this.eats;
+            
+            if(this.score >= this.limit) {
+                this.level += 1;
+                this.limit = this.level * 50;
+                lvl.innerHTML = this.level;
+                board.level = this.level;
+            }
+            
             return true;
         }
         return false;
     }
     
     check_collision() {
+        
+        var idx = this.level <= 5 ? this.level - 2 : 5;
+        
         if(this.body[0][0] >= 25 || this.body[0][0] < 0 || this.body[0][1] >= 15 || this.body[0][1] < 0){
             clearInterval(interval);
             gameOver();
@@ -140,6 +182,16 @@ class Snake {
                 gameOver();
             }
         }
+        
+        if(this.level > 1){
+            for(var i = 0; i < board.obstacles[idx].length; i++){
+                if(this.body[0][0] == board.obstacles[idx][i][0] && this.body[0][1] == board.obstacles[idx][i][1]){
+                    clearInterval(interval);
+                    gameOver();
+                }
+            }   
+        }
+        
     }
 }
 
